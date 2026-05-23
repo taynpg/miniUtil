cdfb12de10edcafba515f2beab02a8a0fadc83c6
taynpg <taynpg@163.com>
2026-05-23 16:46:38 +0800

    添加windows转码。

--- miniUtil.cpp
+++ miniUtil.cpp
@@ -3,10 +3,11 @@
 #include <iostream>
 #include <vector>
 
-#ifdef _WIN32
+#ifdef OS_MINI_WINDOWS
 #include <Windows.h>
 #endif
 
+#ifdef OS_MINI_WINDOWS
 class Utf8ConsoleBuf : public std::streambuf
 {
 protected:
@@ -36,6 +37,7 @@ protected:
         return n;
     }
 };
+#endif
 
 miniUtil::miniUtil()
 {
@@ -47,7 +49,7 @@ miniUtil::~miniUtil()
 
 void miniUtil::setU8Output()
 {
-#ifdef _WIN32
+#ifdef OS_MINI_WINDOWS
     static Utf8ConsoleBuf buf;
     std::cout.rdbuf(&buf);
 #else
@@ -73,4 +75,51 @@ std::vector<std::string> miniUtil::split(const std::string& str, const std::stri
 
     result.emplace_back(str, start, str.size() - start);
     return result;
+}
+
+std::pair<miniErr, std::string> miniUtil::U82Ansi(const std::string& str)
+{
+#ifdef OS_MINI_WINDOWS
+    int wideCharLen = MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, nullptr, 0);
+    if (wideCharLen <= 0) {
+        return {"Error1", ""};
+    }
+    std::wstring wideStr(wideCharLen, L'\0');
+    MultiByteToWideChar(CP_UTF8, 0, str.c_str(), -1, &wideStr[0], wideCharLen);
+    int acpLen = WideCharToMultiByte(CP_ACP, 0, wideStr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+    if (acpLen <= 0) {
+        return {"Error2", ""};
+    }
+    std::string acpStr(acpLen, '\0');
+    WideCharToMultiByte(CP_ACP, 0, wideStr.c_str(), -1, &acpStr[0], acpLen, nullptr, nullptr);
+
+    acpStr.resize(acpLen - 1);
+    return {"", acpStr};
+#else
+    return {"", str};
+#endif
+}
+
+std::pair<miniErr, std::string> miniUtil::Ansi2U8(const std::string& str)
+{
+#ifdef OS_MINI_WINDOWS
+    int wideCharLen = MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, nullptr, 0);
+    if (wideCharLen <= 0) {
+        return {"Error1", ""};
+    }
+    std::wstring wideStr(wideCharLen, L'\0');
+    MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, &wideStr[0], wideCharLen);
+
+    int utf8Len = WideCharToMultiByte(CP_UTF8, 0, wideStr.c_str(), -1, nullptr, 0, nullptr, nullptr);
+    if (utf8Len <= 0) {
+        return {"Error2", ""};
+    }
+    std::string utf8Str(utf8Len, '\0');
+    WideCharToMultiByte(CP_UTF8, 0, wideStr.c_str(), -1, &utf8Str[0], utf8Len, nullptr, nullptr);
+
+    utf8Str.resize(utf8Len - 1);
+    return {"", utf8Str};
+#else
+    return {"", str};
+#endif
 }
--- miniUtil.h
+++ miniUtil.h
@@ -2,15 +2,24 @@
 #define MINI_UTIL_H
 
 #include <string>
+#include <utility>
 #include <vector>
 
 /*
  * 最基本的辅助工具，无需额外依赖。
- * 
- * version: 0.1.0
- * 
-*/
+ *
+ * version: 0.2.0
+ *
+ * 所有 std::pair 返回值的，统一，第一个值是错误字符串，为空正常。
+ */
 
+#ifdef _WIN32
+#define OS_MINI_WINDOWS
+#else
+#define OS_MINI_UNIXLIKE
+#endif
+
+using miniErr = std::string;
 class miniUtil
 {
 public:
@@ -20,6 +29,8 @@ public:
 public:
     static void setU8Output();
     static std::vector<std::string> split(const std::string& str, const std::string& delim);
+    static std::pair<miniErr, std::string> U82Ansi(const std::string& str);
+    static std::pair<miniErr, std::string> Ansi2U8(const std::string& str);
 };
 
 #endif   // MINI_UTIL_H
